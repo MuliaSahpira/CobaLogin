b14d2eae20c5609fcf903c967558c7b2363a4f24
MuliaSahpira <muliasahpira23@mhs.mdp.ac.id>
2023-02-25 12:15:09 +0700

    CobaLogin

--- app/src/main/java/com/example/cobalogin/KendaliLogin.java
+++ app/src/main/java/com/example/cobalogin/KendaliLogin.java
@@ -1,4 +1,55 @@
 package com.example.cobalogin;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class KendaliLogin {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor spe;
+
+    public void setPref(Context ctx, String key, String value)
+    {
+        sp = PreferenceManager.getDefaultSharedPreferences(ctx);
+        spe = sp.edit();
+        spe.putString(key, value);
+        spe.commit();
+    }
+
+    public String getPref(Context ctx, String key)
+    {
+        sp = PreferenceManager.getDefaultSharedPreferences(ctx);
+        //cara langsung
+        return sp.getString(key, null);
+
+        //cara modul
+//        String tampung = sp.getString(key,null);
+//        return tampung;
+    }
+
+    public Boolean isLogin(Context ctx, String key)
+    {
+        //cara langsung
+        if (this.getPref(ctx, key) != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        //cara modul
+//        sp = PreferenceManager.getDefaultSharedPreferences(ctx);
+//        String tampung = sp.getString(key, null);
+//
+//        if (tampung != null)
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+//        }
+    }
 }
--- app/src/main/java/com/example/cobalogin/MainActivity.java
+++ app/src/main/java/com/example/cobalogin/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.cobalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    KendaliLogin KL = new KendaliLogin();
+    private TextView tvSesi;
+    private Button btnLogout;
+    private String ambilSesi;
+    public static String keySPusername = "spPadang";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (KL.isLogin(MainActivity.this, keySPusername) == true)
+        {
+            setContentView(R.layout.activity_main);
+
+            ambilSesi = KL.getPref(MainActivity.this, keySPusername);
+            tvSesi = findViewById(R.id.tv_sesi);
+            tvSesi.setText(ambilSesi);
+            btnLogout = findViewById(R.id.btn_logout);
+        }
+        else
+        {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+    }
+
+    public void prosesLogout(View view)
+    {
+        KL.setPref(MainActivity.this, keySPusername,null);
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 }
